782a473b0de1592cf5c688a34f4a06e7b8540940
eshohag <eshohag@outlook.com>
2021-02-13 12:27:55 +0600

    Update features

---
+++ MySqlConnections/MySql.cs
@@ -0,0 +1,14 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace MySqlConnections
+{
+    public abstract class MySql
+    {
+        public static MySqlConnection Connection(string serverName, string databaseName, string userId, string password)
+        {
+            string connectionString = "SERVER=" + serverName + ";" + "DATABASE=" + databaseName + ";" + "UID=" + userId + ";" + "PASSWORD=" + password + ";";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
